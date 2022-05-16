1ca467ac6b6c264245ee75c5c38f158941fda399
Ricardo-Piedade <ricardo.padade@gmail.com>
2022-05-16 11:56:56 +0100

    Hiding Api Keys

--- Stopitfy.py
+++ Stopitfy.py
@@ -6,9 +6,10 @@ from spotipy.oauth2 import SpotifyOAuth
 from youtube_search import YoutubeSearch
 from tkinter import Tk, filedialog
 import os
+import config
 
-SPOTIPY_CLIENT_ID="752ac9c2479943e480a053168dc04dcf"
-SPOTIPY_CLIENT_SECRET="8f0db783bda04f92bf58b60bd498be06"
+SPOTIPY_CLIENT_ID=config.SPOTIPY_CLIENT_ID
+SPOTIPY_CLIENT_SECRET=config.SPOTIPY_CLIENT_SECRET
 SPOTIPY_REDIRECT_URI="http://localhost:8080"
 SCOPE = "user-top-read"
 
---
+++ config.py
@@ -0,0 +1,4 @@
+# .gitignore should include reference to config.py
+.gitignore 
+SPOTIPY_CLIENT_ID="752ac9c2479943e480a053168dc04dcf"
+SPOTIPY_CLIENT_SECRET="8f0db783bda04f92bf58b60bd498be06"
